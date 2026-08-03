1c07ffc69b490ce593eeea99ae22e74ec5f1775e
muhammadsohaib796 <sohaibshakeel796@gmail.com>
2026-08-03 23:55:49 +0500

    Tools.py created

---
+++ tools.py
@@ -0,0 +1,23 @@
+from langchain.tools import tool 
+import requests
+from bs4 import BeautifulSoup
+from tavily import TavilyClient
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+@tool
+def web_search(query : str) -> str:
+    """Search the web for recent and reliable information on a topic . Returns Titles , URLs and snippets."""
+    results = tavily.search(query=query,max_results=5)
+
+    out = []
+
+    for r in results['results']:
+        out.append(
+            f"Title: {r['title']}\nURL: {r['url']}\nSnippet: {r['content'][:300]}\n"
+        )
+    
+    return "\n----\n".join(out)
