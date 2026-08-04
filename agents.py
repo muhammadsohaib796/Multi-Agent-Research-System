beb8d6f800a91947cf250978442a4426dddd5004
muhammadsohaib796 <sohaibshakeel796@gmail.com>
2026-08-04 13:16:28 +0500

    Build search agent created

---
+++ agents.py
@@ -0,0 +1,19 @@
+from langchain.agents import create_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from tools import web_search , scrape_url 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#model setup (Google AI Studio - free tier Gemini model)
+llm = ChatGoogleGenerativeAI(model = "gemini-2.5-flash", temperature=0)
+
+
+#1st agent 
+def build_search_agent():
+    return create_agent(
+        model = llm,
+        tools= [web_search]
+    )
